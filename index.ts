2023ca4104980597ad2888ca2c05e388988818b4
Mohan <mohanllc091@gmail.com>
2024-03-22 01:31:33 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+
+
+    { message: "Enter your firstnumber", type: "number", name: "firstnumber" },
+    { message: "Enter your secondnumber", type: "number", name: "secondnumber" },
+    {
+
+        message: "select one of operators to perform the action",
+        type: "list",
+        name: "operator",
+        choices: ["addition", "subtraction", "multiplication", "division"]
+    },
+])
+
+    // conditional statement
+    if (answer.operator === "addition") {
+        console.log(answer.firstnumber + answer.secondnumber);
+     
+        
+    }
+   else if (answer.operator === "subtraction") {
+        console.log(answer.firstnumber - answer.secondnumber);
+     
+        
+    }
+    else if (answer.operator === "multiplication") {
+        console.log(answer.firstnumber * answer.secondnumber);
+     
+        
+    }
+    else if (answer.operator === "division") {
+        console.log(answer.firstnumber / answer.secondnumber);
+     
+        
+    }
+    else{"please select correct operator"}
+
